b3faf4702bc410b6e1fed33f13c2bcc3e345521d
Luiz Henrique <luizhenriqueti@outlook.com>
2020-09-29 18:35:54 -0300

    view tela login iniciada

---
+++ view/index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->    
+    <link rel="stylesheet" href="../stylesheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>CSTOKES</title>
+  </head>
+  <body>
+    <div class="container" id="tela-login">
+
+      <div class="d-flex justify-content-center">
+        <h1 id="boas-vindas">Bem vindo ao CStokes, faça seu login!</h1>
+      </div>
+
+      <div class="d-flex align-items-center justify-content-center" id="area-login">
+
+        <div class="d-flex justify-content-center" >
+          
+          <div class="card" id="card-login" style="width: 500px;"> <!-- card --> 
+    
+            <form>
+              <div class="form-group" id="card-input-login">              
+                <input type="text" class="form-control" id="login" aria-describedby="loginHelp" placeholder="LOGIN">    
+              </div>
+              <div class="form-group" id="card-input-senha">
+                <input type="password" class="form-control" id="senha" placeholder="SENHA">
+              </div>
+        
+              <div id="botoes-card-login" class="d-flex align-items-end">
+                <button type="submit" class="btn btn-primary" id="card-btn-login">Login</button>
+                <button type="button" class="btn btn-danger">Recuperar acesso</button>
+              </div>
+            </form>
+           
+          </div><!-- card -->
+    
+        </div>
+
+      </div>
+
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
