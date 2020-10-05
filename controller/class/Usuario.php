d7d1ac4ce6251857234bfe8889b18597604c17b2
Luiz Henrique <luizhenriqueti@outlook.com>
2020-10-05 09:52:34 -0300

    crud refeito e testado, ainda falta o feedback no crud

--- controller/class/Usuario.php
+++ controller/class/Usuario.php
@@ -2,11 +2,71 @@
 
 class Usuario{
 
-	private $id;
-	private $login;
-	private $senha;
-	private $email;
+	//Construtor
+	public function __construct($login = "", $password="", $email=""){
+		
+	}
 
+	//este metodo é chamado quando alguem tenta dar um echo no objeto da sua classe usuario
+	// public function __toString() {
+
+	// 	return json_encode(array(
+	// 		'id' => $this->getId(),
+	// 		'login' => $this->getLogin(),
+	// 		'senha' => $this->getSenha(),
+	// 		'email' => $this->getEmail()
+	// 	));
+
+	// }
+
+	
+
+	//CRUD
+
+	//Create
+	public function create($login, $password, $email){
+		$sql = new Sql();
+		$results = $sql->query(
+			"INSERT INTO usuarios (login, senha, email)
+		 	VALUES ('$login', '$password', '$email')"
+		);
+	}
+
+	//Read
+	public function read($id=""){
+		$sql = new Sql();
+		if (isset($id) && $id != null) {			
+			$results = $sql->select("SELECT * FROM usuarios WHERE ID = $id");
+			return $results;
+		}else {			
+			$results = $sql->select("SELECT * FROM usuarios");
+			return $results;
+		}
+	}
+
+	//Update
+	public function update($id,$dados){
+		$login = $dados['login'];
+		$senha = $dados['senha'];
+		$email = $dados['email'];
+
+        $usuario = new Usuario();
+        $sql = new Sql();
+		
+		$sql->query("UPDATE usuarios 
+		SET login = '$login' , senha = '$senha', email = '$email' 
+		WHERE id = $id");
+	}
+
+	//Delete
+	public function delete($id){
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM usuarios WHERE id = $id");
+	}
+
+
+	//Getters e Setters
 	public function getId(){
 		return $this->id;
 	}
@@ -44,163 +104,7 @@ class Usuario{
 		$this->email = $value;
 	}
 
-
-	//este metodo é chamado quando alguem tenta dar um echo no objeto da sua classe usuario
-	public function __toString() {
-
-		return json_encode(array(
-			'id' => $this->getId(),
-			'login' => $this->getLogin(),
-			'senha' => $this->getSenha(),
-			'email' => $this->getEmail()
-		));
-
-	}
-
-
-
-	//metodo construtor
-	public function __construct($login = "", $password="", $email=""){
-		$this->setLogin($login);
-		$this->setSenha($password);
-		$this->setEmail($email);
-	}
-
-
-	//deleta um usuario
-	public function delete(){
-
-		$sql = new Sql();
-		$sql->query("DELETE FROM usuarios WHERE id = :ID" ,array(
-			":ID" => $this->getIdusuario()
-		));
-
-		$this->setId(0);
-		$this->setSenha("");
-		$this->setLogin("");
-		$this->setEmail("");
-
-	}
-
-
-	//atualiza um usuario existente
-	public function update($arr){
-        $usuario = new Usuario();
-        $sql = new Sql();
-
-        $result = $usuario->buscaPorId($arr['id']);
-      
-       foreach ($arr as $key => $value) {
-           if ($value == "" || $value == null) {            
-                $value = $result[0][$key];
-                $arr[$key] = $value;
-           }
-        }
-    
-       
-      
 	
-		$sql->query("UPDATE usuarios SET login = :LOGIN, senha = :PASSWORD, email = :EMAIL WHERE id = :ID", array(
-		 	":LOGIN" => $arr['login'],
-		 	":PASSWORD" => $arr['senha'],
-		 	":EMAIL" => $arr['email'],
-		 	":ID" => $arr['id']
-        ));
-        
-   
-
-
-	}
-
-
-	//carrega pelo id
-	public function loadById($id){
-		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM usuarios WHERE id = :ID",array(
-			":ID" => $id
-		));
-
-		if (count($results[0]) > 0) {
-			
-			$this->setData($results[0])	;
-
-		}
-    }
-    
-    //busca pelo id
-	public function buscaPorId($id){
-		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM usuarios WHERE id = :ID",array(
-			":ID" => $id
-		));
-
-		return $results;
-	}
-
-
-	//pega tudo da tabela usuarios e orderna pelo login
-	public static function getList(){
-		$sql = new Sql();
-		return $sql->select("SELECT * FROM usuarios ORDER BY login");
-	}
-
-
-	//pesquisa pelo nome 
-	public static function search($login){
-		$sql = new Sql();
-		return $sql->select("SELECT * FROM usuarios WHERE login LIKE :SEARCH ORDER BY login", array(
-			"SEARCH" =>  "%".$login."%"
-		));
-	}
-
-
-	//função para fazer login
-	public function login($login, $password){
-
-		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM usuarios WHERE login = :LOGIN AND senha = :PASSWORD",array(
-			":LOGIN" => $login,
-			":PASSWORD" => $password
-		));
-
-		if (count($results[0]) > 0) {
-			
-			$this->setData($results[0]);			
-
-		} else {
-
-			throw new Exception("Login e/ou senha inválidos.");
-			
-		}
-
-	}
-
-	public function setData($data){
-
-		$this->setId($data['id']);
-		$this->setLogin($data['login']);
-		$this->setSenha($data['senha']);
-        $this->setEmail($data['email']);
-        
-	}
-
-    //insere registros na tabela usuarios
-	public function insert(){
-		$sql = new Sql();
-		$results = $sql->select("INSERT INTO usuarios (login, senha, email) VALUES (:LOGIN, :PASSWORD, :EMAIL)",array(
-			":LOGIN" => $this->getLogin(),
-            ":PASSWORD" => $this->getSenha(),
-            ":EMAIL" => $this->getEmail()
-		));
-
-		if (count($results) > 0) {
-			
-			$this->setData($results[0]);	
-
-		}
-	}
-
-
 }
 	
 
--- controller/teste.php
+++ controller/teste.php
@@ -3,15 +3,12 @@ require_once("../config.php");
 
 $usuario = new Usuario();
 
-$arr = [
-    "id"=> 2,
-    "login" => "teste update",
-    "senha" => "", 
-    "email" => "novomail@mail.com"
-];
+// $usuario->create('novo usuario1','senha1','email1'); //funcionou
+//$result = $usuario->read(); //funcionou
+//$usuario->update(1,array('login'=>'update2','senha'=>'novasenha2','email'=>'novoemail2')); //funcionou
+//$usuario->delete(4); //funcionou
 
-$usuario->update($arr);
-
+echo 'sucesso';
 
 
 
